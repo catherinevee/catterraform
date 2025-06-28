a5ca92e9fd438017d2c43f196390c1330f8b7953
Catherine Vee <catherine.vee@outlook.com>
2025-06-27 18:31:26 -0700

    remove unused db files, update index.html

--- supabase/migrations/20250625194125-maps-services.sql
+++
@@ -1,8 +0,0 @@
-
--- Azure Maps services
--- This migration focuses on Maps and location services
-
-INSERT INTO public.azure_resources (type, category, description, docs_url) VALUES
--- Maps
-('azurerm_maps_account', 'location', 'Manages a Maps Account', 'https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/maps_account'),
-('azurerm_maps_creator', 'location', 'Manages a Maps Creator', 'https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/maps_creator');
